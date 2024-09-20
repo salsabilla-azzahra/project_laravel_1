27902321518158b8c3b9f8070f6c177d28731647
salsabilla-azzahra <salsabillaazzahra3108@gmail.com>
2024-09-20 20:52:10 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"> 
+        <title>Homepage</title> 
+</head> 
+    <body> 
+        <h1 class="position-absolute top-50 start-50 translate middle">Belajar Framework Laravel!</h1> 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> 
+</body> 
+</html> 
